c35119362570bfa0ba6fa7879be616beb86975e6
Nicola Molinari <nicola.molinari@commercetools.de>
2015-06-19 20:50:39 +0200

    Use HashHistory for production (because of gh-pages)

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.join(__dirname, 'dist'),
-    publicPath: ''
+    publicPath: '/'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),

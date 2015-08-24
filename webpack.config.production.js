049442bb732e2bb9f24ae52513a7808885d65303
Nicola Molinari <nicola.molinari@commercetools.de>
2015-08-24 10:30:28 +0200

    Add redux-devtools (for development only)

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -19,7 +19,8 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
-      }
+      },
+      '__DEVTOOLS__': false
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {

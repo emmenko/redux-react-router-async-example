6333eebd2c921e160ab05b35cd4dd3002b3fe7ea
niqdev <niqdev@gmail.com>
2016-03-15 12:36:14 +0100

    draft application tests

--- test/actions/application-test.js
+++ test/actions/application-test.js
@@ -1,7 +1,27 @@
+import { expect } from 'chai';
+import * as constants from '../../lib/constants'
+import * as applicationActions from '../../lib/actions/application'
+
 describe('application actions', () => {
 
-  it('test example', () => {
+  it('test login', () => {
     throw Error('not implemented yet');
+  });
+
+  it('test switchLocale', () => {
+    const LOCALE = 'myLocale'
+    const expectedAction = {
+      type: constants.LOCALE_SWITCHED,
+      payload: LOCALE
+    }
+    expect(applicationActions.switchLocale(LOCALE)).to.deep.equal(expectedAction)
+  });
+
+  it('test hideError', () => {
+    const expectedAction = {
+      type: constants.HIDE_ERROR
+    }
+    expect(applicationActions.hideError()).to.deep.equal(expectedAction)
   });
 
 });
